fe0da7338eb9fb4d828a3f2c8d0c3871f6e9a792
HyojinHwang <hhjin1814@gmail.com>
2023-09-10 15:52:35 +0900

    jwt to X-User header

--- deploy-gateway.sh
+++ deploy-gateway.sh
@@ -2,6 +2,6 @@
 kubectl delete deployments spring-gateway
 docker rmi ppojin/gateway:latest || echo "docker rmi code[$?]"
 docker build --tag ppojin/gateway:latest ./gateway
-minikube image rm -f ppojin/gateway:latest || echo "minikube rmi code [$?]"
+#minikube image list
 minikube image load ppojin/gateway:latest && echo "image load success"
 kubectl apply -f ./kubernetes/apps/gateway.yaml
